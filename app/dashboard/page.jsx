3f030d9e0370d9e97dbb59f62454fed6e76e32fd
Vitalii Honchar <weaxme@gmail.com>
2025-01-11 10:56:48 +0100

    Fixed dashboard page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { createClient } from '@/lib/db/dbServer';
 
 const DashboardPage = async () => {
-  // const supabase = await createServerClient();
+  const supabase = await createClient();
 
-  // const { data: projects } = await supabase.from('projects').select();
+  const { data: projects } = await supabase.from('projects').select();
 
   return (
     <div className="container mx-auto p-4">
@@ -12,11 +13,11 @@ const DashboardPage = async () => {
         <button className="bg-blue-500 text-white px-4 py-2 rounded">➕ Add Project</button>
       </div>
       <ul className="list-disc pl-5">
-        {/* {projects?.map((project) => (
+        {projects?.map((project) => (
           <li key={project.id} className="mb-2">
             {project.name}
           </li>
-        ))} */}
+        ))}
       </ul>
     </div>
   );
