94db96187a9dd1a66599f43129f088fdb7290b33
Vitalii Honchar <weaxme@gmail.com>
2025-01-23 08:42:16 +0100

    Made dashboard page mobile friendly

--- app/application/dashboard/page.jsx
+++ app/application/dashboard/page.jsx
@@ -6,10 +6,9 @@ const DashboardPage = async () => {
   const projects = await projectRepo.getProjectsIdAndNames();
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl font-semibold">Projects</h2>
+    <div className="container mx-auto sm:p-4 lg:p-8">
+      <div className="flex flex-col sm:flex-row justify-between items-center mb-4">
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold mb-4">Projects</h1>
         <AddProjectButton />
       </div>
       <ProjectTable projects={projects} />
