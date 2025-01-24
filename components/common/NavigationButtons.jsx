4602d696bdd4081b0a0391a1e1d7e3a9e752c98a
Vitalii Honchar <weaxme@gmail.com>
2025-01-24 08:16:35 +0100

    Fixed icons

--- components/common/NavigationButtons.jsx
+++ components/common/NavigationButtons.jsx
@@ -1,4 +1,4 @@
-const NavigationButtons = ({ onPrevious, onNext, prevLabel, nextLabel }) => {
+const NavigationButtons = ({ onPrevious, onNext, prevLabel, nextLabel, prevIcon, nextIcon }) => {
     const hasPrevious = !!prevLabel;
     const hasNext = !!nextLabel;
     
@@ -16,6 +16,7 @@ const NavigationButtons = ({ onPrevious, onNext, prevLabel, nextLabel }) => {
                     <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
                     </svg>
+                    {hasPrevious && <span className="mr-2">{prevIcon}</span>}
                     {prevLabel || 'Previous'}
                 </button>
                 <button
@@ -26,6 +27,7 @@ const NavigationButtons = ({ onPrevious, onNext, prevLabel, nextLabel }) => {
                             ? 'text-gray-700 bg-white border border-gray-300 hover:bg-gray-50' 
                             : 'text-gray-400 bg-gray-100 border border-gray-200 cursor-not-allowed'}`}
                 >
+                    {hasNext && <span className="mr-2">{nextIcon}</span>}
                     {nextLabel || 'Next'}
                     <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
