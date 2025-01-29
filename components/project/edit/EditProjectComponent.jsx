4b72e0772cdef4f4e2ca29eb3f9d0b22f46b33d7
Vitalii Honchar <weaxme@gmail.com>
2025-01-29 13:35:03 +0100

    Fixed spinner size

--- components/project/edit/EditProjectComponent.jsx
+++ components/project/edit/EditProjectComponent.jsx
@@ -127,7 +127,7 @@ export default function EditProjectComponent({ project: initialProject }) {
                             {project.name}
                         </h1>
                         {isAnyTaskInProgress() && (
-                            <svg className="animate-spin h-4 w-4 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <svg className="animate-spin h-6 w-6 sm:h-4 sm:w-4 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
