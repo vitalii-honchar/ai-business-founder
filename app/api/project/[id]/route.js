0897d004daa2c9ee3d9c5b169a3a0f374725e489
Vitalii Honchar <weaxme@gmail.com>
2025-02-04 15:25:48 +0100

    Added deleting project

--- app/api/project/[id]/route.js
+++ app/api/project/[id]/route.js
@@ -20,3 +20,21 @@ export async function GET(request, { params }) {
         );
     }
 }
+
+export async function DELETE(request, { params }) {
+    const userId = await getUserId();
+    const { id } = await params;
+    const log = loggerWithProjectId(userId, id);
+    
+    try {
+        if (!id) {
+            return NextResponse.json({ message: 'No projectId provided' }, { status: 400 });
+        }
+        log.info('Deleting project');
+        await projectService.deleteProject(userId, id);
+        return NextResponse.json({ message: 'Project deleted successfully' });
+    } catch (error) {
+        log.error({ error: error.message, stack: error.stack }, 'Error deleting project');
+        return NextResponse.json({ success: false, error: error.message }, { status: error.status || 500 });
+    }
+}
